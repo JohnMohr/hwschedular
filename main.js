00b97018d9d4b9d3a3b16ffc9058c712b6451cab
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2020-12-17 20:43:52 -0800

    added id to savebtn class. got rid of button within saveBtn div. created js on click event. wip

--- main.js
+++ main.js
@@ -1,6 +1,63 @@
-// console.log(dayjs())
-// var events = document.querySelectorAll('.container')
-// console.log(events)
-// // events.forEach(container function(){
-// //     console.log(events)
-// // })
+// // var currentTime = Moment.currentTime()
+// $(document).ready(function () {
+//     // var timeInputs = JSON.parse(localStorage.getItem('timeInputs')) || {};
+//     var currentHour = moment().hours();
+//     var todaysDate = moment().format("dddd, MMMM Do YYYY");
+//     console.log(currentHour);
+//     //Create Variable with the hours.
+//     var timeInputs = [
+//         { time: 7, input: "" },
+//         { time: 8, input: "" },
+//         { time: 9, input: "Wake up" },
+//         { time: 10, input: "" },
+//         { time: 11, input: "Go to work" },
+//         { time: 12, input: "" },
+//         { time: 13, input: "" },
+//         { time: 14, input: "" },
+//         { time: 15, input: "" },
+//         { time: 16, input: "" },
+//         { time: 17, input: "" },
+
+//     ]
+//     //Show Today's date on the DOM
+//     function printTime() {
+//         $("#currentDay").text(todaysDate);
+//     }
+//     //Show Hours on the DOM
+//     function printInputBlocks() {
+//         for (let i = 0; i < timeInputs.length; i++) {
+//             console.log(timeInputs[i].time, timeInputs[i].input);
+//             var inputGroup = $('<div class="input-group mb-3">');
+//             var inputGroupPrepend = $('<div class="input-group-prepend">');
+//             var prependSpan = $('<span class="input-group-text">' + timeInputs[i].time + ':00' + '</span>');
+//             inputGroupPrepend.append(prependSpan);
+//             var inputEl = $('<input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" value="' + timeInputs[i].input + '">');
+//             var inputGroupAppend = $('<div class="input-group-append">');
+//             var appendSpan = $('<span data-time="" class="input-group-text"><button>Save</button></span>');
+//             inputGroupAppend.append(appendSpan);
+//             inputGroup.append(inputGroupPrepend).append(inputEl).append(inputGroupAppend);
+//             $(".container").append(inputGroup);
+//         }
+//     }
+//     function compareTime() {
+//         var nowTime = parseInt(moment().format('HH'));
+//         //Start from 9AM, till 5PM
+//         for (time = 9; time >= 17; time++) {
+//             var timeBlock = parseInt($("#" + time + "hr").attr("data-index"));
+//             console.log(timeBlock);
+//             if (timeBlock < nowTime) {
+//                 $("#" + time + "hr").addClass("past");
+//             } else if (timeBlock == nowTime) {
+//                 $("#" + time + "hr").addClass("present");
+//             } else if (timeBlock > nowTime) {
+//                 $("#" + time + "hr").addClass("future");
+//             }
+//         }
+//     }
+//         compareTime();
+//     printTime();
+//     printInputBlocks();
+// });
+$(".saveBtn").on("click", function(){
+    var dataTime = $(this).attr("data-time")
+})
