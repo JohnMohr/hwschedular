ef4ad4d14ed34cab24149666f01b604d2fc5607a
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2020-12-17 17:25:00 -0800

    defined class to css properties. wip.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-console.log(dayjs())
-var events = document.querySelectorAll('.container')
-console.log(events)
-// events.forEach(container function(){
-//     console.log(events)
-// })
+// console.log(dayjs())
+// var events = document.querySelectorAll('.container')
+// console.log(events)
+// // events.forEach(container function(){
+// //     console.log(events)
+// // })
